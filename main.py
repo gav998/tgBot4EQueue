ea52996d8fc81bd4a4f0b62e9fe11c3cb4bc80d0
Алексей <gav998@yandex.ru>
2020-11-06 11:30:48 +0300

    Простой телеграмм бот для создания электронной очереди. Функция f1_1 ожидает сообщений. При получении сообщения от admin_chat_id вызывается слудующий участник электронной очереди

---
+++ main.py
@@ -0,0 +1,45 @@
+from settings import TOKEN
+from utils import *
+
+import time
+import telebot
+
+bot = telebot.TeleBot(TOKEN)
+
+admin_chat_id = '461258157' # message.chat.id администратора ресурса для управления электронной очередью
+
+queue = []
+
+
+# ожидание команды reg
+@bot.message_handler(commands=['start'])
+def f1_1(message):
+    try:
+        print(message.from_user.id,message.chat.id, message.text, "f1_1")
+        if message.content_type != "text":
+            raise Exception("Ожидалось текстовое сообщение")
+        
+        _id = message.chat.id
+        
+        if _id == admin_chat_id:
+            s=f'В очереди {len(queue)} человек.\n'
+            bot.send_message(admin_chat_id, s)
+            if len(queue) > 0:
+                t = queue.pop(0)
+                bot.send_message(t, "Проходите.\n")
+                bot.send_message(admin_chat_id, f"Участник {t} приглашен.\n")
+        else:
+            queue.append(_id)
+            bot.send_message(_id, f"Ожидайте. Вас вызовут.\nПеред Вами {len(queue)-1} человек.\n")
+
+    except Exception as e:
+        print(e)
+        msg = bot.send_message(message.chat.id,
+                               f'{e}\noooops, попробуйте еще раз..\n\nВведите /reg для продолжения')
+
+
+if __name__ == "__main__":
+    start = time.time()
+    print('start bot')
+    bot.polling()
+    print(time.time() - start)
