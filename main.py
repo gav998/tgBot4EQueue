174ce8c9ecd163f9c8766c0dccba98dbc3202357
Алексей <gav998@yandex.ru>
2020-11-06 12:09:25 +0300

    Добавлено сообщение администратору

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from settings import TOKEN
-from utils import *
 
 import time
 import telebot
 
 bot = telebot.TeleBot(TOKEN)
 
-admin_chat_id = '461258157' # message.chat.id администратора ресурса для управления электронной очередью
+admin_chat_id = 461258157 # message.chat.id администратора ресурса для управления электронной очередью
 
 queue = []
 
@@ -31,6 +30,7 @@ def f1_1(message):
         else:
             queue.append(_id)
             bot.send_message(_id, f"Ожидайте. Вас вызовут.\nПеред Вами {len(queue)-1} человек.\n")
+            bot.send_message(admin_chat_id, f"В очереди {len(queue)} человек.\n")
 
     except Exception as e:
         print(e)
